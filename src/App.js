1c526d727114dc47f0ce1d70dbb0ccca9331c5a4
Steve Clair <64785141+Rex-1031@users.noreply.github.com>
2022-12-10 12:47:28 -0500

    created button, form-input, and signup components, changed sign-in to authentication

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import { Route, Routes } from "react-router-dom";
+import Authentication from "./routes/authentication/authentication.component";
 import Home from "./routes/home/home.component";
 import Navigation from "./routes/navigation/navigation.component";
-import SignIn from "./routes/signin/sign-in.component";
+
 
 const ShopPage =()=>{
   return(
@@ -15,7 +16,7 @@ const  App = ()=> {
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home/>} />
         <Route path="/shop" element={<ShopPage />}/>
-        <Route path="/sign-in" element={<SignIn/>}/>
+        <Route path="/auth" element={<Authentication/>}/>
       </Route>
     </Routes>
 
--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -1,9 +1,11 @@
+
+import SignUpForm from "../../components/sign-up/sign-up-form.components";
 import { 
     signInWithGooglePopup,
     createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils"
 
-const SignIn =()=>{
+const Authentication =()=>{
     const logGoogleUser = async () =>{
         const {user} = await signInWithGooglePopup();
         const userDocRef = await createUserDocumentFromAuth(user)
@@ -14,10 +16,13 @@ const SignIn =()=>{
             <button onClick={logGoogleUser}>
                 Sign in with Google
             </button>
+
+            <SignUpForm />
+            
         </div>
     )
 }
 
-export default SignIn
+export default Authentication
 
 
