01250d8a64850f804691026291e645a8ac273727
Steve Clair <64785141+Rex-1031@users.noreply.github.com>
2022-12-21 11:48:05 -0500

    more work on the shopping cart feature

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,13 +1,23 @@
+import { useContext } from 'react'
+import { CartContext } from '../../contexts/cart.context'
 import Button from '../button/button.component'
 import CartItem from '../cart-item/cart-item.component'
 import './cart-dropdown.styles.scss'
 
 const CartDropdown =()=>{
+    const { cartItems } = useContext(CartContext)
     return(
         <div className='cart-dropdown-container'>
             <div className='cart-items'>
+           
                 {
-                    [].map(item=> <CartItem cartItem={item}/>)
+                    cartItems.length? (
+                        cartItems.map(item=> (
+                            <CartItem key={item.id} cartItem={item}/>
+                        ))
+                    ):(
+                        <span className='empty-message'>Your Cart is Empty</span>
+                    )
                 }
             </div>
             <Button>GO TO CHECKOUT</Button>
