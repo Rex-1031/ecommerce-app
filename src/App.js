fd5b97254e9a59134a96069c3080be90aaf72571
Steve Clair <64785141+Rex-1031@users.noreply.github.com>
2022-12-27 18:43:33 -0500

    completed checkout funcitonality and styled pages

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navigation from "./routes/navigation/navigation.component";
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import Shop from "./routes/shop/shop.component";
+import Checkout from "./routes/checkout/checkout.component";
 
 library.add(fab)
 
@@ -16,6 +17,7 @@ const  App = ()=> {
         <Route index element={<Home/>} />
         <Route path="/shop" element={<Shop />}/>
         <Route path="/auth" element={<Authentication/>}/>
+        <Route path="checkout" element={<Checkout />} />
       </Route>
     </Routes>
 
--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,4 +1,5 @@
 import { useContext } from 'react'
+import { useNavigate } from 'react-router-dom'
 import { CartContext } from '../../contexts/cart.context'
 import Button from '../button/button.component'
 import CartItem from '../cart-item/cart-item.component'
@@ -6,21 +7,23 @@ import './cart-dropdown.styles.scss'
 
 const CartDropdown =()=>{
     const { cartItems } = useContext(CartContext)
+    const navigate = useNavigate()
+
+    const checkoutHandler = ()=>{
+        navigate('/checkout')
+    }
     return(
         <div className='cart-dropdown-container'>
             <div className='cart-items'>
            
                 {
-                    cartItems.length? (
+                    
                         cartItems.map(item=> (
                             <CartItem key={item.id} cartItem={item}/>
                         ))
-                    ):(
-                        <span className='empty-message'>Your Cart is Empty</span>
-                    )
                 }
             </div>
-            <Button>GO TO CHECKOUT</Button>
+            <Button onClick={checkoutHandler}>GO TO CHECKOUT</Button>
         </div>
     )
 }
